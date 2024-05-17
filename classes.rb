6d5e1b2784bea3e44ee5a7606f2ed9cccc6adaf2
steffi <steffi.amper@gmail.com>
2024-05-17 11:21:26 -0500

    lecture files

---
+++ classes.rb
@@ -0,0 +1,17 @@
+class Student
+  def instance_greeting
+    puts "Hello, this is an instance method!"
+    
+  end
+
+  def self.class_greeting
+    puts "I am a class method"
+  end
+end
+
+bob = Student.new()
+
+bob.instance_greeting
+Student.class_greeting
+
+bob.class_greeting
